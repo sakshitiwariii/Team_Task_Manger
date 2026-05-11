c1a0158cb379ef570564a3d4c30a7fc97c8e8ebb
Sakshi <sakshitiwari2441@gmail.com>
2026-05-11 17:30:28 +0530

    .

--- client/src/api/axiosClient.js
+++ client/src/api/axiosClient.js
@@ -1,14 +1,18 @@
 import axios from "axios";
 
 const axiosClient = axios.create({
-  baseURL: import.meta.env.VITE_API_URL || "https://teamtaskmanger-production-a7a3.up.railway.app/"
+  baseURL:
+    import.meta.env.VITE_API_URL ||
+    "https://teamtaskmanger-production-a7a3.up.railway.app/api",
 });
 
 axiosClient.interceptors.request.use((config) => {
   const token = localStorage.getItem("ttm_token");
+
   if (token) {
     config.headers.Authorization = `Bearer ${token}`;
   }
+
   return config;
 });
 
