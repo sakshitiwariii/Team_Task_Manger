6c9138e41bbd06fff4b01e4682da3bf7b6fba3db
Sakshi <sakshitiwari2441@gmail.com>
2026-05-11 18:47:42 +0530

    Axios correction

--- client/src/api/axiosClient.js
+++ client/src/api/axiosClient.js
@@ -1,8 +1,9 @@
 import axios from "axios";
 
 const axiosClient = axios.create({
-  baseURL: import.meta.env.VITE_API_BASE_URL || "http://localhost:5000/api",
-  withCredentials: true
+  baseURL:
+    import.meta.env.VITE_API_URL ||
+    "https://teamtaskmanger-production-a7a3.up.railway.app/api",
 });
 
 axiosClient.interceptors.request.use((config) => {
